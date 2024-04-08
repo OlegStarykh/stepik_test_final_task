4b295507d6de2eb4de2b4157445f4abed8c89377
oldboy <starikxxxl@mail.ru>
2024-04-08 14:05:39 +0300

    Добавил в base_page.py метод is_element_present "Проверка наличия элемента"

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,19 @@
 from .base_page import BasePage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
+    def go_to_login_page(self, timeout=10):
         login_link = self.driver.find_element('xpath', '//*[@id="login_link"]')
         login_link.click()
+
+
+    def should_be_login_link(self):
+        wait = WebDriverWait(self.driver, 20, 1)
+        login_link = ('xpath', '//*[@id="login_link"]')
+        assert self.is_element_present(*login_link), "Login link is not presented"
+
+
+
